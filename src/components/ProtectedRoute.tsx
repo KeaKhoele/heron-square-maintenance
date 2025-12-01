8d59c63c5067cca8a5484f19e0a21dfad45c058a
Kea Khoele <kea.khoele@gmail.com>
2025-12-01 15:29:14 +0200

    Remove unused useAccessControl import from ProtectedRoute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
-import { useAccessControl } from '../contexts/AccessControlContext';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
